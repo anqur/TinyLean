1814f9b3fb18601c0e85c8c3116d7a230562d9cd
anqurvanillapy <anqurvanillapy@gmail.com>
2025-01-26 16:22:19 +0800

    Scaffolding
    Signed-off-by: anqurvanillapy <anqurvanillapy@gmail.com>

---
+++ .github/README.md
@@ -0,0 +1,7 @@
+# TinyLean
+
+Tiny theorem prover in Python, with syntax like Lean 4.
+
+## License
+
+MIT
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,10 +1,6 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
-
 import unittest
 
-from sample.simple import add_one
+from TinyLean.simple import add_one
 
 
 class TestSimple(unittest.TestCase):
@@ -13,5 +9,5 @@ class TestSimple(unittest.TestCase):
         self.assertEqual(add_one(5), 6)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
