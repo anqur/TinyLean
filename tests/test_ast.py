a8efc300eb6a5511f824643ff5d724d5672798f5
anqurvanillapy <anqurvanillapy@gmail.com>
2025-02-03 16:25:29 +0800

    Register custom parse action
    Signed-off-by: anqurvanillapy <anqurvanillapy@gmail.com>

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from TinyLean import Ident
+from TinyLean import ast, Ident, grammar
 
 
 class TestIdent(TestCase):
@@ -8,7 +8,8 @@ class TestIdent(TestCase):
         self.assertNotEqual(Ident.fresh("i").id, Ident.fresh("j").id)
 
 
-class TestAST(TestCase):
-    def test_parse_program(self):
-        # TODO
-        pass
+class TestParser(TestCase):
+    def test_parse_name(self):
+        i = grammar.NAME.parse_string("hello", parse_all=True)[0]
+        self.assertEqual(type(i), Ident)
+        self.assertEqual(i.text, "hello")
